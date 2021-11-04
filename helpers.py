09f3c6e3db26e4cbf3a9a0fe4d108467a567c964
FelixRottler <fero429@gmail.com>
2021-11-04 08:39:52 +0100

    small cleanup

--- helpers.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def rand_pos(size_x, size_y):
-    x = np.random.choice(size_x // 10)
-    y = np.random.choice(size_y // 10)
-    pos_x = [10 * i for i in range(size_x // 10)]
-    pos_y = [10 * i for i in range(size_y // 10)]
-    return pos_x[x], pos_y[y]
